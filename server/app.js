b3767fabad7d2bb75ab45f1ce0f90674742787fe
Amanda Jopp <aclaire488@gmail.com>
2017-03-07 19:08:45 -0600

    changed server port

--- server/app.js
+++ server/app.js
@@ -41,6 +41,6 @@ MongoDB.once('open', function () {
 });
 
 //server
-app.listen('3000', function(){
-  console.log('listening on 3000');
+app.listen('8080', function(){
+  console.log('listening on 8080');
 });
